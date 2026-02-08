919e96f6af3bbf55edf5412b8b7c08143b7f5901
moeztouilll <moez.touil@esprit.tn>
2026-02-08 06:24:06 +0100

    Update settings.py

--- backend/pay4all/settings.py
+++ backend/pay4all/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8yhq7+mg+v&(zrp#@(7zpp)_#$h)j*spe--j2_7!q8e!t=zc4*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
@@ -154,18 +154,14 @@ REST_FRAMEWORK = {
 }
 
 # CORS Configuration
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://espritmaratech2026-overclock-4.onrender.com",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 # CSRF Configuration
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://espritmaratech2026-overclock-4.onrender.com",
+    "https://espritmaratech2026-overclock-6.onrender.com",
 ]
 
 # Cookie Settings for Dev
