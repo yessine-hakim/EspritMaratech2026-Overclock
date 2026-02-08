0097919e98676821998890078d48a830dfdefa26
moeztouilll <moez.touil@esprit.tn>
2026-02-08 09:35:21 +0100

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000', // Django server URL
+    baseURL: import.meta.env.VITE_API_BASE_URL || 'https://espritmaratech2026-overclock-6.onrender.com', // Django server URL
     withCredentials: true, // Important for session cookies
     headers: {
         'Content-Type': 'application/json',
